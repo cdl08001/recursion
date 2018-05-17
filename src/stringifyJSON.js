35b79da21242b25af7d11579015a188783e9b87b
Calin Lewis <calin.lewis@gmail.com>
2018-05-16 21:37:36 -0700

    Update stringify.JSON.js (V1)
    Two functions built: isSimpleObject and isArray

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,57 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
-};
+/*
+Step 1: create function identifyObjectType(obj)
+  - Given an object, identify the type of object
+  - and if it is a 'number', 'boolean', or 'string',
+  - return it as a value;
+
+Step 2: create function isArray(obj)
+  - Given an object, identify if it is an array
+  - if so,
+
+
+
+Base Case: if Object.keys(obj).length is 0, return result
+
+Recursive Case:
+  - Run .shift() on keys array (pulling it off and modifying array), store in variable.
+  - Return obj[variable] to capture value at given key, push to variable
+  - Add .shift() value to result.
+  - Add semicolon string ': ' (include space) to result
+  - Add obj[variable] to result
+  - delete obj[variable]
+  - rerun stringifyJSON with object.
+  - if Oject.keys(obj).length === 1, we need to close our result with '}'.
+
+*/
+
+var result = '';
+
+function isSimpleObject(obj){
+  if(typeof obj === 'number'){
+    return result + obj;
+  } else if(typeof obj === 'boolean'){
+    return result + obj;
+  } else if(typeof obj === 'string'){
+    return result + obj;
+  } else if (obj === null){
+    return result + obj;
+  }
+}
+
+function isArray(obj){
+  if(Array.isArray(obj)){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var stringifyJSON = function(obj){
+  //Base Case:
+
+
+  //Recursive Case:
+}
