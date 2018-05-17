b881e2870b2a589e7b636b03ef2a621784a130e4
Calin Lewis <calin.lewis@gmail.com>
2018-05-16 23:28:16 -0700

    Update stringify.JSON.js (V3)
    Changed logic of isSimpleObject to return
    boolean value as opposed to the result itself.
    This makes it easier to pass into stringifyJSON.
    
    Also added base and recursive case logic to
    stringifyJSON.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,13 +1,11 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
-
 // but you don't so you're going to write it from scratch:
-
 /*
 Step 1: create function identifyObjectType(obj)
   - Given an object, identify the type of object
   - and if it is a 'number', 'boolean', or 'string',
-  - return it as a value;
+  - return it as a string;
 
 Step 2: create function isArray(obj)
   - Given an object, identify if it is an array, and if so, check if empty.
@@ -16,8 +14,21 @@ Step 2: create function isArray(obj)
     - If empty, return '{}', otherwise, return false;
 
 Base Case: 
+  - If array.length === 0, OR
+  - Object.keys(obj).length === 0, add ] or } to result.
+  - Return result
 
 Recursive Case:
+  - If object is an array:
+    - use obj.shift() to target first element. Add to result.
+    - call stringifyJSON(obj) to work through remaining elements
+
+  - If object is not an array (meaning it is an object literal):
+    - use Object.keys(obj) to get an array of the object keys. Push to variable.
+    - add first element in array to result, followed by ": ", followed by obj[key] value, followed by ", ".
+    - remove first element key from object using delete.
+    - call stringifyJSON(obj) to work through remaining elements
+
 
 
 */
@@ -26,13 +37,13 @@ var result = '';
 
 function isSimpleObject(obj){
   if(typeof obj === 'number'){
-    return result + obj;
+    return true;
   } else if(typeof obj === 'boolean'){
-    return result + obj;
+    return true;
   } else if(typeof obj === 'string'){
-    return result + obj;
+    return true;
   } else if (obj === null){
-    return result + obj;
+    return true;
   }
 }
 
@@ -51,8 +62,31 @@ function isArray(obj){
 }
 
 var stringifyJSON = function(obj){
-  //Base Case:
 
+  isSimpleObject(obj);
+  var objectIsArray = Array.isArray(obj);
+
+  //Base Case:
+  if(isSimpleObject){
+    result += obj;
+    return result; 
+  } else if(obj.length === 0){
+    result += ']';
+    return result;
+  } else if(Object.keys(obj).length === 0){
+    result += '}';
+    return result;
+  } 
 
   //Recursive Case:
+  if(objectIsArray){
+    var currrentElement = obj.shift();
+    result += currrentElement;
+    stringifyJSON(obj);
+  } else if(!objectIsArray){
+    var objectKeys = Object.keys(obj);
+    result += objectKeys[0] + ': ' + obj[objectKeys[0]] + ', ';
+    delete obj[objectKeys[0]];
+    stringifyJSON(obj);
+  }
 }
