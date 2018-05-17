8357b3fece855889b4f364555f8dbda848aab141
Calin Lewis <calin.lewis@gmail.com>
2018-05-16 21:52:18 -0700

    Update stringify.JSON (V2)
    Added functionality to isArray to return
    an empty array or object literal if one
    was found.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -10,22 +10,15 @@ Step 1: create function identifyObjectType(obj)
   - return it as a value;
 
 Step 2: create function isArray(obj)
-  - Given an object, identify if it is an array
-  - if so,
+  - Given an object, identify if it is an array, and if so, check if empty.
+    - If empty, return '[]', otherwise, return true;
+  - If object is NOT an array, check if empty.
+    - If empty, return '{}', otherwise, return false;
 
-
-
-Base Case: if Object.keys(obj).length is 0, return result
+Base Case: 
 
 Recursive Case:
-  - Run .shift() on keys array (pulling it off and modifying array), store in variable.
-  - Return obj[variable] to capture value at given key, push to variable
-  - Add .shift() value to result.
-  - Add semicolon string ': ' (include space) to result
-  - Add obj[variable] to result
-  - delete obj[variable]
-  - rerun stringifyJSON with object.
-  - if Oject.keys(obj).length === 1, we need to close our result with '}'.
+
 
 */
 
@@ -45,9 +38,15 @@ function isSimpleObject(obj){
 
 function isArray(obj){
   if(Array.isArray(obj)){
-    return true;
+    if(obj.length === 0){
+      return '[]';
+    } else {
+      return true;
+    } 
+  } else if(Object.keys(obj).length === 0){
+      return '{}';
   } else {
-    return false;
+      return false; 
   }
 }
 
