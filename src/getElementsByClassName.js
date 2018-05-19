7cc33521b66ea06c44d49ee7cb07514424a1aae6
Calin Lewis <calin.lewis@gmail.com>
2018-05-19 10:30:00 -0700

    Final Results: getElementsByClassName.js
    Completed assignment

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,45 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-};
+/*
+
+1) document.body
+   - Example: <body>(all body elements)</body>
+
+2) element.childNodes
+   - Example: [text, div#mocha, text, div.targetClassName]
+
+3) element.classList
+   - Example: childNodes[3].classList might equal something like: ["targetClassName", value: "targetClassName"]
+
+Input: 'targetClassName'
+   - Example: <div class="targetClassName"></div>
+
+Output: Array containing all elements with a matching class:
+   - Example: [body.targetClassName, div.targetClassName]
+
+Need to use underbar to leverage .contains and .forEach:
+
+Steps:
+1) Create nodeList variable to hold results.
+2) Create function to find child nodes with matching classes:
+   - if a node's classList contains the className, push to results.
+   - if a node contains children, run function recursively, pushing each
+     child node as input to test function.
+3) Return results. 
+*/
+
+function getElementsByClassName(className) {
+  var nodeList = [];
+  function findMatchingClasses(node) {
+    if (_(node.classList).contains(className)) {
+      nodeList.push(node);
+    }
+    _(node.childNodes).forEach(function(child) {
+      findMatchingClasses(child);
+    });
+  }
+  findMatchingClasses(document.body);
+  return nodeList;
+}
+
